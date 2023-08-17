e30508cf98265a6e71270a74ea42ffa0f3efd14b
Junxiong Wang <chuangzhetianxia@gmail.com>
2023-08-17 13:56:16 -0700

    Create tpu_pip_install.sh
    update pip install in tpu

---
+++ tpu_pip_install.sh
@@ -0,0 +1,8 @@
+pip install chex==0.1.5
+pip install "jax[tpu]==0.3.14" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install transformers==4.27.1
+pip install flax==0.5.0
+pip install optax==0.1.3
+pip install scikit-learn==1.3.0
+pip install wandb==0.13.4
+pip install datasets==2.14.4
