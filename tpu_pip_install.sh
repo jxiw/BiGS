777958725421bce0a5f39691785f76723fad7bf3
Junxiong Wang <chuangzhetianxia@gmail.com>
2023-08-18 21:22:40 -0700

    Update tpu_pip_install.sh

--- tpu_pip_install.sh
+++ tpu_pip_install.sh
@@ -1,5 +1,5 @@
 pip install chex==0.1.5
-pip install "jax[tpu]==0.3.14" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install "jax[tpu]==0.3.13" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install transformers==4.27.1
 pip install flax==0.5.0
 pip install optax==0.1.3
